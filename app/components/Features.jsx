a48cc0d4e5d90b4673c2db29f18e622e0f9fd6fd
radouan larabi <116183732+radlarabi@users.noreply.github.com>
2025-01-17 19:51:55 +0000

    add animation onScroll

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -1,11 +1,42 @@
 import Image from "next/image"
 import { MdOutlineFileDownload } from "react-icons/md";
-
+import { motion, useScroll, useTransform, useSpring } from 'motion/react'
+import { useRef } from "react";
 
 export default function Features(){
+    const scrollRefF1 = useRef(null)
+    const scrollRefF3 = useRef(null)
+    const loopNames = ["CHAT", "HANG OUT", "TALK", "PLAY"]
+
+    const { scrollYProgress: scrollYProgressFeatue1  } = useScroll({
+        target: scrollRefF1,
+        offset: ["start end", "end start"]
+    })
+    const { scrollYProgress: scrollYProgressFeatue3  } = useScroll({
+        target: scrollRefF3,
+        offset: ["start end", "end start"]
+    })
+    const scaleYF1 = useTransform(scrollYProgressFeatue1, [0, 1], [0, 200]);
+    const fasterTranslateYF1 = useSpring(scaleYF1, 
+        {
+            stiffness: 50,
+            damping: 20,
+            mass: 0.5,
+        }
+    )
+
+    const scaleYF3 = useTransform(scrollYProgressFeatue3, [0, 1], [0, 200]);
+    const fasterTranslateYF3 = useSpring(scaleYF3, 
+        {
+            stiffness: 50,
+            damping: 20,
+            mass: 0.5,
+        }
+    )
+
     return (
-        <>
-            <div className="feature_1 relative flex flex-col md:flex-row-reverse justify-center items-center">
+        <div className="relative">
+            <div ref={scrollRefF1} className="feature_1 relative flex flex-col md:flex-row-reverse justify-center items-center">
                 <div className="absolute top-[-60px] md:right-[25%] -z-10 h-[60px] overflow-hidden ">
                     <Image
                         src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6620ec7544fa3849c3cb27fc_party_wumpus.gif"
@@ -24,6 +55,17 @@ export default function Features(){
                         <source src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/663b271d6f05c8c9e11f8d65_Discord%20Refresh%20Sound-MP4-transcode.mp4"/>
                     </video>
                 </div>
+                <motion.div className="absolute top-[-60px] md:left-[-20%] -z-10"
+                    style={{ translateY: fasterTranslateYF1}}
+                >
+                    <Image
+                        src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664d8350b243428e2289b09e_dis_Icon_Egg%20(1).webp"
+                        width="450"
+                        height="450"
+                        alt="abc"
+                        className=""
+                    />
+                </motion.div>
             </div>
 
             <div className="feature_2 flex flex-col md:flex-row justify-center items-center">
@@ -38,8 +80,8 @@ export default function Features(){
                 </div>
             </div>
 
-            <div className="feature_3 relative flex flex-col md:flex-row-reverse justify-center items-center">
-                <div className="absolute md:top-[-92px] md:right-[-2.4rem] top-[-68px] right-[-1.8rem]">
+            <div ref={scrollRefF3} className="feature_3 relative flex flex-col md:flex-row-reverse justify-center items-center">
+                <div className="absolute md:top-[-92px] md:right-[-2.4rem] top-[-68px] right-[-1.8rem] z-10">
                     <Image
                         src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6630f482123160b94617877a_Box%20(1).webp"
                         width="200"
@@ -52,12 +94,74 @@ export default function Features(){
                     <div className="font-[abcgintonord] text-[2rem]">Hop in when you're free, no need to call</div>
                     <div className="mb-8 mt-4">Easily hop in and out of voice or text chats without having to call or invite anyone, so your party chat lasts before, during, and after your game session.</div>
                 </div>
-                <div className="md:w-[50%]">
+                <div className="md:w-[50%] z-10">
                     <video className="rounded-3xl aspect-[435/335] max-w-[100%] object-cover" autoPlay loop playsInline muted>
                         <source src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6638c6b04eff56a99c1e2d7d_Discord_Website_Refresh_Hop-In-transcode.mp4"/>
                     </video>
                 </div>
+                <motion.div className="absolute top-[-20rem] left-[5rem] -z-100"
+                    style={{ translateY: fasterTranslateYF3}}
+                >
+                    <Image
+                        src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664dac894dc189990b0c7a53_dis_icon_Rupee_01%201.webp"
+                        width="200"
+                        height="200"
+                        alt="abc"
+                        className=""
+                    />
+                </motion.div>
+
+                <motion.div className="absolute bottom-[-1rem] right-[0rem] -z-100"
+                    style={{ translateY: fasterTranslateYF3}}
+                >
+                    <Image
+                        src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664dac2db27f64223ce5f6f9_Discord%20Icons_Bat%201.webp"
+                        width="200"
+                        height="200"
+                        alt="abc"
+                        className=""
+                    />
+                </motion.div>
+                {/* https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/664dac2db27f64223ce5f6f9_Discord%20Icons_Bat%201.webp */}
             </div>
+
+
+            <div className="mx-[-2rem] w-[100vw] h-[140px] ">
+
+            </div>
+            <div className="z-10  w-[100vw] ">
+                <motion.div className="flex whitespace-nowrap h-[140px] z-10 gap-[5rem] "
+                    animate={{
+                        x: ["0%", '-50%'],
+                    }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 20,
+                        ease: "linear"
+                    }}
+                >
+                    {
+                        Array.from({length: 3}).map((_, index) => {
+                            return loopNames.map((el, ind) => {
+                                return  (
+                                    <div key={el + index + ind} className="flex justify-center items-center gap-[5rem] ">
+                                        <div className="text-[61px] font-[abcgintonord] ">
+                                            {el}
+                                        </div>
+                                        <Image
+                                            src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/665786b8a93285bff36e194e_discord-mark-white%202.webp"
+                                            width="50"
+                                            height="50"
+                                            alt="discord"
+                                            className="h-[22px] w-[28px]"
+                                        />
+                                    </div>)
+                            })
+                        })
+                    }
+                </motion.div>
+            </div>
+            {/* https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/665786b8a93285bff36e194e_discord-mark-white%202.webp */}
 
             <div className="feature_3 flex flex-col md:flex-row justify-center items-center">
                 <div className="flex flex-col px-8 md:w-[50%]">
@@ -119,14 +223,6 @@ export default function Features(){
                     Download for Linux
                 </button>
             </div>
-            {/* <Image
-                src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6658cc069d1eb1caf9426914_Footer-Art_cut.webp"
-                height='1000'
-                width='1000'
-                alt="al"
-                className=""
-            />
-            <img className="w-[100vw]" src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6658cc069d1eb1caf9426914_Footer-Art_cut.webp"/> */}
-        </>
+        </div>
     )
 }
